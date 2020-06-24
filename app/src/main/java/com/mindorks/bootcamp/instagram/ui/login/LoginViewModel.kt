b5b6bbbb376a8777d5c54aa18f5a3101fc6ef04f
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-24 15:14:55 +0530

    add validation logic to LoginViewModel.kt and logic for calling it from LoginActivity.kt

--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModel.kt
@@ -5,8 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.mindorks.bootcamp.instagram.data.repository.UserRepository
 import com.mindorks.bootcamp.instagram.ui.base.BaseViewModel
-import com.mindorks.bootcamp.instagram.utils.common.Resource
-import com.mindorks.bootcamp.instagram.utils.common.Validation
+import com.mindorks.bootcamp.instagram.utils.common.*
 import com.mindorks.bootcamp.instagram.utils.network.NetworkHelper
 import com.mindorks.bootcamp.instagram.utils.rx.SchedulerProvider
 import io.reactivex.disposables.CompositeDisposable
@@ -17,6 +16,8 @@ class LoginViewModel(
     networkHelper: NetworkHelper,
     private val userRepository: UserRepository
 ) : BaseViewModel(schedulerProvider, compositeDisposable, networkHelper) {
+
+    val launchDummy: MutableLiveData<Event<Map<String, String>>> = MutableLiveData()
 
     private val validationsList: MutableLiveData<List<Validation>> = MutableLiveData()
 
@@ -35,7 +36,39 @@ class LoginViewModel(
                 ?: Resource.unknown()
         }
 
-    override fun onCreate() {
+    override fun onCreate() {}
 
+    fun onEmailChange(email: String) = emailField.postValue(email)
+
+    fun onPasswordChange(email: String) = passwordField.postValue(email)
+
+    fun onLogin() {
+        val email = emailField.value
+        val password = passwordField.value
+
+        val validations = Validator.validateLoginFields(email, password)
+        validationsList.postValue(validations)
+
+        if (validations.isNotEmpty() && email != null && password != null) {
+            val successValidation = validations.filter { it.resource.status == Status.SUCCESS }
+            if (successValidation.size == validations.size && checkInternetConnectionWithMessage()) {
+                loggingIn.postValue(true)
+                compositeDisposable.addAll(
+                    userRepository.doUserLogin(email, password)
+                        .subscribeOn(schedulerProvider.io())
+                        .subscribe(
+                            {
+                                userRepository.saveCurrentUser(it)
+                                loggingIn.postValue(false)
+                                launchDummy.postValue(Event(emptyMap()))
+                            },
+                            {
+                                handleNetworkError(it)
+                                loggingIn.postValue(false)
+                            }
+                        )
+                )
+            }
+        }
     }
 }
