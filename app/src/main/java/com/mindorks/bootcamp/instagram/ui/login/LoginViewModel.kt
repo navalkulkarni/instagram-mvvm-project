6e30dac0f27401403771e3df8a7dc61c6d6cf830
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-24 15:01:31 +0530

    add structure for LoginRequest.kt and LoginResponse.kt. Also, make post call through UserRepository.kt

--- app/src/main/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModel.kt
@@ -1,7 +1,12 @@
 package com.mindorks.bootcamp.instagram.ui.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.mindorks.bootcamp.instagram.data.repository.UserRepository
 import com.mindorks.bootcamp.instagram.ui.base.BaseViewModel
+import com.mindorks.bootcamp.instagram.utils.common.Resource
+import com.mindorks.bootcamp.instagram.utils.common.Validation
 import com.mindorks.bootcamp.instagram.utils.network.NetworkHelper
 import com.mindorks.bootcamp.instagram.utils.rx.SchedulerProvider
 import io.reactivex.disposables.CompositeDisposable
@@ -12,6 +17,23 @@ class LoginViewModel(
     networkHelper: NetworkHelper,
     private val userRepository: UserRepository
 ) : BaseViewModel(schedulerProvider, compositeDisposable, networkHelper) {
+
+    private val validationsList: MutableLiveData<List<Validation>> = MutableLiveData()
+
+
+    val emailField: MutableLiveData<String> = MutableLiveData()
+    val passwordField: MutableLiveData<String> = MutableLiveData()
+    val loggingIn: MutableLiveData<Boolean> = MutableLiveData()
+
+    val emailValidation: LiveData<Resource<Int>> = filterValidation(Validation.Field.EMAIL)
+    val passwordValidation: LiveData<Resource<Int>> = filterValidation(Validation.Field.PASSWORD)
+
+    private fun filterValidation(field: Validation.Field) =
+        Transformations.map(validationsList) {
+            it.find { validation -> validation.field == field }
+                ?.run { return@run this.resource }
+                ?: Resource.unknown()
+        }
 
     override fun onCreate() {
 
