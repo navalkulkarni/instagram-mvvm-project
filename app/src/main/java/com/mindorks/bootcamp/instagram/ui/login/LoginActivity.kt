e99cc0c86192b72dc69a1f991a8c5d58e70196b8
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-24 12:56:33 +0530

    add structure of LoginActivity.kt and LoginViewModel.kt

---
+++ app/src/main/java/com/mindorks/bootcamp/instagram/ui/login/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.mindorks.bootcamp.instagram.ui.login
+
+import android.os.Bundle
+import com.mindorks.bootcamp.instagram.R
+import com.mindorks.bootcamp.instagram.di.component.ActivityComponent
+import com.mindorks.bootcamp.instagram.ui.base.BaseActivity
+
+class LoginActivity : BaseActivity<LoginViewModel>() {
+
+    companion object {
+        const val TAG = "LoginActivity"
+    }
+
+    override fun provideLayoutId(): Int = R.layout.activity_login
+
+    override fun injectDependencies(activityComponent: ActivityComponent) = activityComponent.inject(this)
+
+    override fun setupView(savedInstanceState: Bundle?) {
+
+    }
+}
