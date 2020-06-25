23677a2021b307ed132aa268b69ae9603fd4976e
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-25 15:59:03 +0530

    add test for server responds with success

--- app/src/test/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/mindorks/bootcamp/instagram/ui/login/LoginViewModelTest.kt
@@ -2,18 +2,23 @@ package com.mindorks.bootcamp.instagram.ui.login
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.mindorks.bootcamp.instagram.data.model.User
 import com.mindorks.bootcamp.instagram.data.repository.UserRepository
 import com.mindorks.bootcamp.instagram.utils.common.Event
 import com.mindorks.bootcamp.instagram.utils.common.Resource
 import com.mindorks.bootcamp.instagram.utils.network.NetworkHelper
 import com.mindorks.bootcamp.instagram.utils.rx.TestSchedulerProvider
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.TestScheduler
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -57,6 +62,27 @@ class LoginViewModelTest {
         loginViewModel.launchDummy.observeForever(launchDummyObserver)
         loginViewModel.messageStringId.observeForever(messageStringIdObserver)
 
+    }
+
+    @Test
+    fun givenServerResponse200_whenLogin_shouldLaunchDummyActivity(){
+        val email = "test@gmail.com"
+        val password= "password"
+        val user = User("1",name = "test",email = email, accessToken = "accessToken")
+        loginViewModel.emailField.value = email
+        loginViewModel.passwordField.value = password
+        doReturn(true).`when`(networkHelper).isNetworkConnected()
+        doReturn(Single.just(user))
+            .`when`(userRepository)
+            .doUserLogin(email, password)
+        loginViewModel.onLogin()
+        testScheduler.triggerActions()
+        verify(userRepository).saveCurrentUser(user)
+        assert(loginViewModel.loggingIn.value == false)
+        assert(loginViewModel.launchDummy.value == Event(hashMapOf<String,String>()))
+        verify(loggingInObserver).onChanged(true)
+        verify(loggingInObserver).onChanged(false)
+        verify(launchDummyObserver).onChanged(Event(hashMapOf()))
     }
 
     @After
